614e8d1b4205f05096d02b9d0f76a16ef3610051
AgustinaElenaFernand <fernandez.agustinaelena@gmail.com>
2023-03-23 09:15:23 +0100

    Added changes in src files and package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,17 +15,14 @@ export class AppComponent {
   decPassword:string= "";
   conversionEncryptOutput: string= "";
   conversionDecryptOutput:string= "";
-    
+  encryptedVars:string [] =  [];
+
   constructor() {  
   }  
-  //method is used to encrypt and decrypt the text  
+ 
   convertText(conversion:string) {  
-      if (conversion=="encrypt") {  
-        this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
-      }  
-      else {  
-        this.conversionDecryptOutput = CryptoJS.AES.decrypt(this.encryptText.trim(), this.decPassword.trim()).toString(CryptoJS.enc.Utf8);  
-       
-    }  
-  }  
+    this.conversionEncryptOutput = CryptoJS.AES.encrypt(this.plainText.trim(), this.encPassword.trim()).toString();  
+}
+
+
 }
